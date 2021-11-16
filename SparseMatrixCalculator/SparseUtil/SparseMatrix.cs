3ec33bb2ab6fcbb687cdda3724d2c58bc8ee7a18
AmirSina Mashayekh <pajuhesh80@outlook.com>
2021-11-16 20:30:10 +0330

    Added Add, Subtract and Multiply methods Added documentation comments

--- SparseMatrixCalculator/SparseUtil/SparseMatrix.cs
+++ SparseMatrixCalculator/SparseUtil/SparseMatrix.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SparseMatrixCalculator.SparseUtil
 {
@@ -15,19 +9,43 @@ namespace SparseMatrixCalculator.SparseUtil
     /// <typeparam name="T">The type of elements in matrix.</typeparam>
     public class SparseMatrix
     {
+        /// <summary>
+        /// Count of non-zero elements in matrix.
+        /// </summary>
         public readonly int elementsCount;
 
-        public readonly int totalRows;
+        /// <summary>
+        /// Count of rows in the original matrix.
+        /// </summary>
+        public readonly int originalRowsCount;
 
-        public readonly int totalColumns;
+        /// <summary>
+        /// Count of columns in the original matrix.
+        /// </summary>
+        public readonly int originalColumnsCount;
 
+        /// <summary>
+        /// Contains index of each non-zero element in the original matrix.
+        /// First column indicates original row and second column indicates original column.
+        /// </summary>
         public readonly int[,] indexes;
 
+        /// <summary>
+        /// Contains non-zero elements of matrix.
+        /// </summary>
         public readonly double[] elements;
 
-        public SparseMatrix(int elementsCount, int totalRows, int totalColumns)
+        /// <summary>
+        /// Initializes a new instance of the <c>SparseMatrix</c> class
+        /// that has the specified capacity.
+        /// </summary>
+        /// <param name="elementsCount">Count of non-zero elements.</param>
+        /// <param name="originalRowsCount">Count of rows in the original matrix.</param>
+        /// <param name="originalColumnsCount">Count of columns in the original matrix.</param>
+        /// <exception cref="ArgumentException"></exception>
+        public SparseMatrix(int elementsCount, int originalRowsCount, int originalColumnsCount)
         {
-            if (elementsCount > totalRows * totalColumns)
+            if (elementsCount > originalRowsCount * originalColumnsCount)
             {
                 throw new ArgumentException("elementsCount should not be " +
                     "more than totalRows multiplied by totalColumns.", "elementsCount");
@@ -36,33 +54,38 @@ namespace SparseMatrixCalculator.SparseUtil
             {
                 throw new ArgumentException("elementsCount should not be less than zero.", "elementsCount");
             }
-            if (totalRows < 0)
+            if (originalRowsCount < 0)
             {
                 throw new ArgumentException("totalRows should not be less than zero.", "totalRows");
             }
-            if (totalColumns < 0)
+            if (originalColumnsCount < 0)
             {
                 throw new ArgumentException("totalColumns should not be less than zero.", "totalColumns");
             }
 
             this.elementsCount = elementsCount;
-            this.totalRows = totalRows;
-            this.totalColumns = totalColumns;
+            this.originalRowsCount = originalRowsCount;
+            this.originalColumnsCount = originalColumnsCount;
             indexes = new int[elementsCount, 2];
             elements = new double[elementsCount];
         }
 
+        /// <summary>
+        /// Creates new instance of the <c>SparseMatrix</c> class
+        /// from a matrix.
+        /// </summary>
+        /// <param name="matrix">A two dimentional <c>double</c> array which contains matrix data.</param>
         public SparseMatrix(double[,] matrix)
         {
-            totalRows = matrix.GetLength(0);
-            totalColumns = matrix.GetLength(1);
-            int[,] tmpIndexes = new int[totalRows * totalColumns, 2];
-            double[] tmpValues = new double[totalRows * totalColumns];
+            originalRowsCount = matrix.GetLength(0);
+            originalColumnsCount = matrix.GetLength(1);
+            int[,] tmpIndexes = new int[originalRowsCount * originalColumnsCount, 2];
+            double[] tmpValues = new double[originalRowsCount * originalColumnsCount];
             elementsCount = -1;
 
-            for (int i = 0; i < totalRows; i++)
+            for (int i = 0; i < originalRowsCount; i++)
             {
-                for (int j = 0; j < totalColumns; j++)
+                for (int j = 0; j < originalColumnsCount; j++)
                 {
                     double element = matrix[i, j];
                     if (element != 0)
@@ -88,8 +111,25 @@ namespace SparseMatrixCalculator.SparseUtil
             }
         }
 
-        public double Get(int row, int col)
+        /// <summary>
+        /// Gets element at a specific index in matrix.
+        /// </summary>
+        /// <param name="row">The row of element.</param>
+        /// <param name="col">The column of element.</param>
+        /// <returns>
+        /// The element at specified index.
+        /// If element isn't defined in this <c>SparseMatrix</c>
+        /// but is in range of original matrix, returns zero.
+        /// </returns>
+        /// <exception cref="ArgumentException">Index is out of range of original matrix.</exception>
+        public double GetElementAt(int row, int col)
         {
+            if (row < 0 || row >= originalRowsCount ||
+                col < 0 || col >= originalColumnsCount)
+            {
+                throw new ArgumentException("Index is out of range of original matrix.");
+            }
+
             int len = indexes.GetLength(0);
             for (int i = 0; i < len; i++)
             {
@@ -99,61 +139,49 @@ namespace SparseMatrixCalculator.SparseUtil
                 }
             }
 
-            throw new ArgumentException();
+            return 0;
         }
 
-        public void Sort()
+        /// <summary>
+        /// Converts this instance of <c>SparseMatrix</c> to a normal matrix.
+        /// </summary>
+        /// <returns>A two dimentional <c>double</c> array which contains matrix data.</returns>
+        public double[,] ToMatrix()
         {
-            for (int i = 0; i < elementsCount; i++)
+            double[,] result = new double[originalRowsCount, originalColumnsCount];
+
+            for (int i = 0; i < originalRowsCount; i++)
             {
-                int currentRow = indexes[i, 0];
-                int currentColumn = indexes[i, 1];
-                int lowestValueIndex = i;
-
-                for (int j = i + 1; j < elementsCount; j++)
+                for (int j = 0; j < originalColumnsCount; j++)
                 {
-                    int cmpRow = indexes[j, 0];
-                    int cmpColumn = indexes[j, 1];
-
-                    if (cmpRow < currentRow ||
-                        (cmpRow == currentRow && cmpColumn < currentColumn))
-                    {
-                        lowestValueIndex = j;
-                        currentRow = cmpRow;
-                        currentColumn = cmpColumn;
-                    }
-                }
-
-                if (lowestValueIndex != i)
-                {
-                    // Swap indexes
-                    indexes[lowestValueIndex, 0] = indexes[i, 0];
-                    indexes[lowestValueIndex, 1] = indexes[i, 1];
-                    indexes[i, 0] = currentRow;
-                    indexes[i, 1] = currentColumn;
-
-                    // Swap elements
-                    double tmp = elements[i];
-                    elements[i] = elements[lowestValueIndex];
-                    elements[lowestValueIndex] = tmp;
+                    result[i, j] = GetElementAt(i, j);
                 }
             }
+
+            return result;
         }
 
+        /// <summary>
+        /// Returns the Transpose of matrix.
+        /// </summary>
+        /// <param name="matrix">An instance of <c>SparseMatrix</c> class.</param>
+        /// <returns>
+        /// An instance of <c>SparseMatrix</c> class which indicates transpose of <paramref name="matrix"/>.
+        /// </returns>
         public static SparseMatrix Transpose(SparseMatrix matrix)
         {
             if (matrix.elementsCount == 0) { return matrix; }
 
-            SparseMatrix transposed = new SparseMatrix(matrix.elementsCount, matrix.totalRows, matrix.totalColumns);
+            SparseMatrix transposed = new SparseMatrix(matrix.elementsCount, matrix.originalColumnsCount, matrix.originalRowsCount);
 
-            int[] rowSize = new int[matrix.totalRows];
+            int[] rowSize = new int[matrix.originalRowsCount];
             for (int i = 0; i < matrix.elementsCount; i++)
             {
                 rowSize[matrix.indexes[i, 0]]++;
             }
 
-            int[] startOfRow = new int[matrix.totalRows];
-            for (int i = 1; i < matrix.totalRows; i++)
+            int[] startOfRow = new int[matrix.originalRowsCount];
+            for (int i = 1; i < matrix.originalRowsCount; i++)
             {
                 startOfRow[i] = startOfRow[i - 1] + rowSize[i - 1];
             }
@@ -169,6 +197,98 @@ namespace SparseMatrixCalculator.SparseUtil
                 startOfRow[SOR_Index]++;
             }
             return transposed;
+        }
+
+        /// <summary>
+        /// Adds two matrices and returns the result.
+        /// </summary>
+        /// <param name="left">The first matrix.</param>
+        /// <param name="right">The second matrix.</param>
+        /// <returns>Sum of two matrices.</returns>
+        /// <exception cref="ArgumentException">Matrices are not of same size.</exception>
+        public static SparseMatrix Add(SparseMatrix left, SparseMatrix right)
+        {
+            if (left.originalRowsCount != right.originalRowsCount
+                || left.originalColumnsCount != right.originalColumnsCount)
+            {
+                throw new ArgumentException("Matrices are not of same size.");
+            }
+
+            double[,] result = new double[left.originalRowsCount, left.originalColumnsCount];
+
+            for (int i = 0; i < left.elementsCount; i++)
+            {
+                result[left.indexes[i, 0], left.indexes[i, 1]] = left.elements[i];
+            }
+
+            for (int i = 0; i < right.elementsCount; i++)
+            {
+                result[right.indexes[i, 0], right.indexes[i, 1]] += right.elements[i];
+            }
+
+            return new SparseMatrix(result);
+        }
+
+        /// <summary>
+        /// Subtracts one matrix from another and returns the result.
+        /// </summary>
+        /// <param name="left">The matrix to be subtracted.</param>
+        /// <param name="right">The matrix to subtract.</param>
+        /// <returns>The result of subtracting <paramref name="right"/> from <paramref name="right"/></returns>
+        /// <exception cref="ArgumentException">Matrices are not of same size.</exception>
+        public static SparseMatrix Subtract(SparseMatrix left, SparseMatrix right)
+        {
+            if (left.originalRowsCount != right.originalRowsCount
+                || left.originalColumnsCount != right.originalColumnsCount)
+            {
+                throw new ArgumentException("Matrices are not of same size.");
+            }
+
+            double[,] result = new double[left.originalRowsCount, left.originalColumnsCount];
+
+            for (int i = 0; i < left.elementsCount; i++)
+            {
+                result[left.indexes[i, 0], left.indexes[i, 1]] = left.elements[i];
+            }
+
+            for (int i = 0; i < right.elementsCount; i++)
+            {
+                result[right.indexes[i, 0], right.indexes[i, 1]] -= right.elements[i];
+            }
+
+            return new SparseMatrix(result);
+        }
+
+        /// <summary>
+        /// Returns the product of two matrices.
+        /// </summary>
+        /// <param name="left">The first matrix.</param>
+        /// <param name="right">The second matrix.</param>
+        /// <returns>The product of the <paramref name="left"/> and <paramref name="right"/> parameters.</returns>
+        /// <exception cref="ArgumentException">
+        /// The number of columns in the <paramref name="left"/> are not
+        /// equal to the number of rows in the <paramref name="right"/>.
+        /// </exception>
+        public static SparseMatrix Multiply(SparseMatrix left, SparseMatrix right)
+        {
+            if (left.originalColumnsCount != right.originalRowsCount)
+            {
+                throw new ArgumentException("The number of columns in the left matrix are not " +
+                    "equal to the number of rows in the right matrix");
+            }
+
+            double[,] result = new double[left.originalRowsCount, right.originalColumnsCount];
+            for (int i = 0; i < left.elementsCount; i++)
+            {
+                int row = left.indexes[i, 0];
+                int col = left.indexes[i, 1];
+                for (int j = 0; j < right.originalColumnsCount; j++)
+                {
+                    result[row, j] += left.elements[i] * right.GetElementAt(col, j);
+                }
+            }
+
+            return new SparseMatrix(result);
         }
     }
 }
--- SparseMatrixCalculatorTests/SparseUtil/SparseMatrixTests.cs
+++ SparseMatrixCalculatorTests/SparseUtil/SparseMatrixTests.cs
@@ -1,42 +1,104 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SparseMatrixCalculator.SparseUtil;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static SparseMatrixCalculator.SparseUtil.SparseMatrix;
 
 namespace SparseMatrixCalculator.SparseUtil.Tests
 {
     [TestClass()]
     public class SparseMatrixTests
     {
+        private static readonly double[,] matrix1 =
+        {
+            {1, 2, 3},
+            {0, 5, 6},
+            {5, 0, 7},
+            {6, 7, 0},
+            {7, 8, 9}
+        };
+
+        private static readonly SparseMatrix sparseMatrix1;
+
+        private static readonly double[,] matrix2 =
+        {
+            {7, 8, 9},
+            {1, 2, 3},
+            {0, 5, 6},
+            {5, 0, 7},
+            {6, 7, 0}
+        };
+
+        private static readonly SparseMatrix sparseMatrix2;
+
+        private static readonly double[,] matrix3 =
+        {
+            {7, 8, 9, 4},
+            {1, 2, 3, 8},
+            {0, 5, 6, 3}
+        };
+
+        private static readonly SparseMatrix sparseMatrix3;
+
+        private static readonly double[,] matrix1plus2;
+
+        private static readonly double[,] matrix1minus2;
+
+        private static readonly double[,] matrix1mul3;
+
+        static SparseMatrixTests()
+        {
+            int m1r = matrix1.GetLength(0);
+            int m1c = matrix1.GetLength(1);
+            int m3c = matrix3.GetLength(1);
+
+            matrix1plus2 = new double[m1r, m1c];
+            for (int i = 0; i < m1r; i++)
+            {
+                for (int j = 0; j < m1c; j++)
+                {
+                    matrix1plus2[i, j] = matrix1[i, j] + matrix2[i, j];
+                }
+            }
+
+            matrix1minus2 = new double[m1r, m1c];
+            for (int i = 0; i < m1r; i++)
+            {
+                for (int j = 0; j < m1c; j++)
+                {
+                    matrix1minus2[i, j] = matrix1[i, j] - matrix2[i, j];
+                }
+            }
+
+            matrix1mul3 = new double[m1r, m3c];
+            for (int i = 0; i < m1r; i++)
+            {
+                for (int j = 0; j < m3c; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < m1c; k++)
+                    {
+                        sum += matrix1[i, k] * matrix3[k, j];
+                    }
+                    matrix1mul3[i, j] = sum;
+                }
+            }
+
+            sparseMatrix1 = new SparseMatrix(matrix1);
+            sparseMatrix2 = new SparseMatrix(matrix2);
+            sparseMatrix3 = new SparseMatrix(matrix3);
+        }
+
         [TestMethod()]
         public void SparseMatrixFromMatrixTest()
         {
-            double[,] matrix =
-            {
-                {1, 2, 3},
-                {0, 5, 6},
-                {5, 0, 7},
-                {6, 7, 0},
-                {7, 8, 9}
-            };
+            SparseMatrix sparseMatrix = new SparseMatrix(matrix1);
 
-            SparseMatrix sparseMatrix = new SparseMatrix(matrix);
-
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            int m1r = matrix1.GetLength(0);
+            int m1c = matrix1.GetLength(1);
+            for (int i = 0; i < m1r; i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < m1c; j++)
                 {
-                    if (matrix[i, j] == 0)
-                    {
-                        _ = Assert.ThrowsException<ArgumentException>(() => sparseMatrix.Get(i, j));
-                    }
-                    else
-                    {
-                        Assert.AreEqual(matrix[i,j], sparseMatrix.Get(i, j));
-                    }
+                    Assert.AreEqual(matrix1[i, j], sparseMatrix.GetElementAt(i, j));
                 }
             }
         }
@@ -44,29 +106,66 @@ namespace SparseMatrixCalculator.SparseUtil.Tests
         [TestMethod()]
         public void TransposeTest()
         {
-            double[,] matrix =
-            {
-                {1, 2, 3},
-                {0, 5, 6},
-                {5, 0, 7},
-                {6, 7, 0},
-                {7, 8, 9}
-            };
+            SparseMatrix sparseMatrix = SparseMatrix.Transpose(new SparseMatrix(matrix1));
 
-            SparseMatrix sparseMatrix = SparseMatrix.Transpose(new SparseMatrix(matrix));
-
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            int m1r = matrix1.GetLength(0);
+            int m1c = matrix1.GetLength(1);
+            for (int i = 0; i < m1r; i++)
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
+                for (int j = 0; j < m1c; j++)
                 {
-                    if (matrix[i, j] == 0)
-                    {
-                        _ = Assert.ThrowsException<ArgumentException>(() => sparseMatrix.Get(j, i));
-                    }
-                    else
-                    {
-                        Assert.AreEqual(matrix[i, j], sparseMatrix.Get(j, i));
-                    }
+                    Assert.AreEqual(matrix1[i, j], sparseMatrix.GetElementAt(j, i));
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            _ = Assert.ThrowsException<ArgumentException>(() => Add(sparseMatrix1, sparseMatrix3));
+            SparseMatrix sum = Add(sparseMatrix1, sparseMatrix2);
+
+            int m1r = matrix1.GetLength(0);
+            int m1c = matrix1.GetLength(1);
+            for (int i = 0; i < m1r; i++)
+            {
+                for (int j = 0; j < m1c; j++)
+                {
+                    Assert.AreEqual(matrix1plus2[i, j], sum.GetElementAt(i, j));
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            _ = Assert.ThrowsException<ArgumentException>(() => Subtract(sparseMatrix1, sparseMatrix3));
+            SparseMatrix dif = Subtract(sparseMatrix1, sparseMatrix2);
+
+            int m1r = matrix1.GetLength(0);
+            int m1c = matrix1.GetLength(1);
+            for (int i = 0; i < m1r; i++)
+            {
+                for (int j = 0; j < m1c; j++)
+                {
+                    Assert.AreEqual(matrix1minus2[i, j], dif.GetElementAt(i, j));
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            _ = Assert.ThrowsException<ArgumentException>(() => Multiply(sparseMatrix1, sparseMatrix2));
+            SparseMatrix mul = Multiply(sparseMatrix1, sparseMatrix3);
+
+            int m3r = matrix3.GetLength(0);
+            int m3c = matrix3.GetLength(1);
+            for (int i = 0; i < m3r; i++)
+            {
+                for (int j = 0; j < m3c; j++)
+                {
+                    Assert.AreEqual(matrix1mul3[i, j], mul.GetElementAt(i, j));
                 }
             }
         }
